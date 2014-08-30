ad4fad25368e7bc5e674d7a34fb12e714cb9a100
LucasMontanheiro <lucasmontanheiro10@gmail.com>
2014-08-30 13:56:39 -0300

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(){
+     
+    int distancia, resultado;
+     
+    scanf("%d",&distancia);
+     
+    resultado = distancia * 2;
+     
+    printf("%d minutos\n", resultado);
+}
