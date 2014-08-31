bb3c8d61b4c2a1fd0c47cf93d99cdd9f508f185b
LucasMontanheiro <lucasmontanheiro10@gmail.com>
2014-08-31 01:28:37 -0300

    Create 1036.c
    No dev não mostra erro, mas o URI não aceita.

---
+++ 1036.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+	float a, b, c, x1, x2, delta;
+	
+	scanf("%f %f %f", &a, &b, &c);
+	
+	delta = b*b - 4*a*c;
+	x1 = (-b + sqrt(delta)) / (2*a);
+	x2 = (-b - sqrt(delta)) / (2*a);
+	
+	if (delta < 0){
+		printf("Impossivel calcular\n");
+	}else{
+		printf("R1 = %.5f\n", x1);
+		printf("R2 = %.5f\n", x2);
+	}
+}
