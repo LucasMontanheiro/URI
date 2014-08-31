d38f175a6641c7cdad88252ee6e3ae2392e3eef1
LucasMontanheiro <lucasmontanheiro10@gmail.com>
2014-08-31 02:14:33 -0300

    Create 1038.c

---
+++ 1038.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(){
+	int codigo, quantidade;
+	float valor;
+	
+	scanf("%d %d", &codigo, &quantidade);
+	
+	if (codigo == 1){
+		valor = quantidade * 4;
+	}
+	if (codigo == 2){
+		valor = quantidade * 4.5;
+	}
+	if (codigo == 3){
+		valor = quantidade * 5;
+	}
+	if (codigo == 4){
+		valor = quantidade * 2;
+	}
+	if (codigo == 5){
+		valor = quantidade * 1.5;
+	}
+	
+	printf("Total: R$ %.2f",valor);
+}
